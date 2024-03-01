377672852ccef20dd35e80d64d854418f64a06fe
bk-gill <132428715+bk-gill@users.noreply.github.com>
2024-03-01 16:58:13 -0700

    added xcode setup

---
+++ Improving Nutrition for Medically Complex Children/Improving Nutrition for Medically Complex Children/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Improving Nutrition for Medically Complex Children
+//
+//  Created by Gill on 2024-03-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
